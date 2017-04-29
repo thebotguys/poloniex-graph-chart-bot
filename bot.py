577f7ea1a1bca4061f7edc9b7222af218b4862af
= <saninoale@gmail.com>
2017-04-29 18:32:50 +0200

    refactoring

--- bot.py
+++ bot.py
@@ -21,90 +21,96 @@ def parse_join(message):
         #print '\033[91m' + str(m) + '\033[0m'
         if (receivedMessage['type'] == 'channel_joined'):
             print '\033[91m I JOINED A CHANNEL \033[0m'
-            chan = receivedMessage['channel']['id']
-            req = rtm_open_channel()
-            params = {
-              'channel' : chan,
-              'token' : TOKEN,
-              'text' : 'Hello to everybody, looks like you need my help in this channel, what can I do for you?',
-              'parse' : 'full',
-              'as_user' : 'true'
-            }
-            resp = requests.post('https://slack.com/api/chat.postMessage', params=params)
+            try:
+                chan = receivedMessage['channel']['id']
+                req = rtm_open_channel()
+                params = {
+                  'channel' : chan,
+                  'token' : TOKEN,
+                  'text' : 'Hello to everybody, looks like you need my help in this channel, what can I do for you?',
+                  'parse' : 'full',
+                  'as_user' : 'true'
+                }
+                resp = requests.post('https://slack.com/api/chat.postMessage', params=params)
+            except Exception as ex:
+                print ex
         elif (receivedMessage['type'] == 'message') and receivedMessage['user'] != BOT_ID:
             print '\033[91m MESSAGE RECEIVED \033[0m'
-            receivedText = receivedMessage['text']
-            chan = receivedMessage['channel']
-            if '@' + BOT_ID in receivedText: #message for me
-                if 'help' in receivedText:
-                    req = rtm_open_channel(channel=chan)
-                    params = {
-                      'channel' : chan,
-                      'token' : TOKEN,
-                      'text' : 'You can ask me any graph by using \n '+
-                               '`@' + BOT_NAME + ' graph [COIN1] [COIN2] [TIME]`\n'+
-                               'where `TIME` is 24h, 7d, 30d, 1y. \n' +
-                               'And of course sir. `COIN1` and `COIN2` are coins\n' +
-                               'Example of call may be `@' + BOT_NAME + ' graph ARK USD 24h`',
-                      'parse' : 'full',
-                      'as_user' : 'true'
-                    }
-                    resp = requests.post('https://slack.com/api/chat.postMessage', params=params)
-                    print '\033[91m HELP MESSAGE POSTED \033[0m'
-                elif 'graph' in receivedText:
-                    try:
-                        message_args = receivedText.split(' ')
+            try:
+                receivedText = receivedMessage['text']
+                chan = receivedMessage['channel']
+                if '@' + BOT_ID in receivedText: #message for me
+                    if 'help' in receivedText:
+                        req = rtm_open_channel(channel=chan)
                         params = {
                           'channel' : chan,
                           'token' : TOKEN,
-                          'attachments' : None,
+                          'text' : 'You can ask me any graph by using \n '+
+                                   '`@' + BOT_NAME + ' graph [COIN1] [COIN2] [TIME]`\n'+
+                                   'where `TIME` is 24h, 7d, 30d, 1y. \n' +
+                                   'And of course sir. `COIN1` and `COIN2` are coins\n' +
+                                   'Example of call may be `@' + BOT_NAME + ' graph ARK USD 24h`',
                           'parse' : 'full',
                           'as_user' : 'true'
                         }
-                        coin1 = message_args[2].lower()
-                        coin2 = message_args[3].lower()
-                        timeframe = message_args[4].lower()
+                        resp = requests.post('https://slack.com/api/chat.postMessage', params=params)
+                        print '\033[91m HELP MESSAGE POSTED \033[0m'
+                    elif 'graph' in receivedText:
+                        try:
+                            message_args = receivedText.split(' ')
+                            params = {
+                              'channel' : chan,
+                              'token' : TOKEN,
+                              'attachments' : None,
+                              'parse' : 'full',
+                              'as_user' : 'true'
+                            }
+                            coin1 = message_args[2].lower()
+                            coin2 = message_args[3].lower()
+                            timeframe = message_args[4].lower()
 
-                        if not timeframe in ['24h', '7d', '30d', '1y']:
-                            response_text = 'Invalid time frame sir. , the available options are : [24h, 7d, 30d, 1y]\n.'
-                            response_text += 'Please ask me more by typing `@' + BOT_NAME + ' help`'
-                        else: #tries to get image
-                            #building url
-                            url = 'https://cryptohistory.org/charts/candlestick/'
-                            url += coin1 + '-' + coin2 + '/' + timeframe + '/png'
-                            resp = requests.get(url)
-                            if resp.status_code == requests.codes.ok:
-                                response_text = "Here you are, sir."
-                                title = coin1.upper() + ' - ' + coin2.upper() + ' '
-                                if (timeframe == '24h'):
-                                    title += '24 Hours'
-                                elif (timeframe == '7d'):
-                                    title += '7 Days'
-                                elif (timeframe == '30d'):
-                                    title += '30 Days'
-                                elif (timeframe == '7d'):
-                                    title += '1 Year'
+                            if not timeframe in ['24h', '7d', '30d', '1y']:
+                                response_text = 'Invalid time frame sir. , the available options are : [24h, 7d, 30d, 1y]\n.'
+                                response_text += 'Please ask me more by typing `@' + BOT_NAME + ' help`'
+                            else: #tries to get image
+                                #building url
+                                url = 'https://cryptohistory.org/charts/candlestick/'
+                                url += coin1 + '-' + coin2 + '/' + timeframe + '/png'
+                                resp = requests.get(url)
+                                if resp.status_code == requests.codes.ok:
+                                    response_text = "Here you are, sir."
+                                    title = coin1.upper() + ' - ' + coin2.upper() + ' '
+                                    if (timeframe == '24h'):
+                                        title += '24 Hours'
+                                    elif (timeframe == '7d'):
+                                        title += '7 Days'
+                                    elif (timeframe == '30d'):
+                                        title += '30 Days'
+                                    elif (timeframe == '7d'):
+                                        title += '1 Year'
+                                    else:
+                                        title += 'Invalid Timeframe [please contact my developer to fix this]'
+                                    title += ' graph'
+                                    params['attachments'] = json.dumps([
+                                        {
+                                            'pretext' : response_text,
+                                            'fallback': 'Crypto Graph',
+                                            'color': '#36a64f',
+                                            'title': title,
+                                            'image_url': url,
+                                            'thumb_url': url
+                                        }
+                                    ])
+                                    print str(params)
                                 else:
-                                    title += 'Invalid Timeframe [please contact my developer to fix this]'
-                                title += ' graph'
-                                params['attachments'] = json.dumps([
-                                    {
-                                        'pretext' : response_text,
-                                        'fallback': 'Crypto Graph',
-                                        'color': '#36a64f',
-                                        'title': title,
-                                        'image_url': url,
-                                        'thumb_url': url
-                                    }
-                                ])
-                                print str(params)
-                            else:
-                                response_text = 'Sorry sir., but I can\'t find the coin pair you are asking for.\n'
-                                response_text += 'Please have in mind that I get data from Poloniex archives.'
-                                params['text'] = response_text
-                            resp = requests.post('https://slack.com/api/chat.postMessage', params=params)
-                    except Exception as ex:
-                        print ex
+                                    response_text = 'Sorry sir., but I can\'t find the coin pair you are asking for.\n'
+                                    response_text += 'Please have in mind that I get data from Poloniex archives.'
+                                    params['text'] = response_text
+                                resp = requests.post('https://slack.com/api/chat.postMessage', params=params)
+                        except Exception as ex:
+                            print ex
+                except Exception as ex:
+                    print ex
         elif(receivedMessage['type'] == 'hello'):
             print '\033[91m HELLO RECEIVED \033[0m'
         else:
@@ -124,7 +130,7 @@ def parse_join(message):
                 print ex
     except Exception as ex:
         print '\033[91m Exception : Message => ' + str(receivedMessage) + '\n Error :' + ex + ' \033[0m'
-        
+
 def start_rtm():
     """Connects to Slacks and initiates socket handshake, returns a websocket"""
     req = requests.get('https://slack.com/api/rtm.start?token='+TOKEN, verify=False)
