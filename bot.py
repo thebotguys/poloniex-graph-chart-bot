f509f38116e02f3ac982b1b22272c403a8ff8325
= <saninoale@gmail.com>
2017-04-29 11:04:54 +0200

    refactoring

--- bot.py
+++ bot.py
@@ -25,8 +25,7 @@ def parse_join(message):
             params = {
               'channel' : chan,
               'token' : TOKEN,
-              'text' : 'Hello to everybody, looks like you need my help \
-                        in this channel, what can I do for you?',
+              'text' : 'Hello to everybody, looks like you need my help in this channel, what can I do for you?',
               'parse' : 'full',
               'as_user' : 'true'
             }
@@ -42,10 +41,7 @@ def parse_join(message):
                     params = {
                       'channel' : chan,
                       'token' : TOKEN,
-                      'text' : '[TODO]You can ask me any graph by using @' + BOT_NAME +
-                               ' graph [COIN1] [COIN2] [TIME], \
-                                 where TIME is 24h, 7d, 30d, 1y. And of course sir. COIN1 \
-                                 and COIN2 are coins',
+                      'text' : '[TODO]You can ask me any graph by using @' + BOT_NAME + ' graph [COIN1] [COIN2] [TIME], where TIME is 24h, 7d, 30d, 1y. \nAnd of course sir. COIN1 and COIN2 are coins',
                       'parse' : 'full',
                       'as_user' : 'true'
                     }
