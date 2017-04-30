6ef513c26678f5f34351fdc761fb4c9e23d3a1d5
= <saninoale@gmail.com>
2017-04-30 17:37:59 +0200

    refactoring

--- bot.py
+++ bot.py
@@ -35,8 +35,8 @@ def parse_join(message):
             #except Exception as ex:
             #    print ex
         elif (received_message['type'] == 'message') and received_message['user'] != BOT_ID:
-                print '\033[91m MESSAGE RECEIVED \033[0m'
             #try:
+                print '\033[91m MESSAGE RECEIVED \033[0m'
                 received_text = received_message['text']
                 chan = received_message['channel']
                 if '@' + BOT_ID in received_text: #message for me
@@ -66,62 +66,67 @@ def parse_join(message):
                             params = {
                               'channel' : chan,
                               'token' : TOKEN,
+                              'text' : None,
                               'attachments' : None,
                               'parse' : 'full',
                               'as_user' : 'true'
                             }
-                            coin1 = message_args[2].lower()
-                            coin2 = message_args[3].lower()
-                            timeframe = message_args[4].lower()
+                            if len(message_args) == 5:
+                                coin1 = message_args[2].lower()
+                                coin2 = message_args[3].lower()
+                                timeframe = message_args[4].lower()
 
-                            if not timeframe in ['24h', '7d', '30d', '1y']:
-                                response_text = 'Invalid time frame sir. , the available options are : [24h, 7d, 30d, 1y]\n.'
-                                response_text += 'Please ask me more by typing `@' + BOT_NAME + ' help`'
-                            else: #tries to get image
-                              #try:
-                                #building url
-                                url = 'https://cryptohistory.org/charts/candlestick/'
-                                url += coin1 + '-' + coin2 + '/' + timeframe + '/png'
-                                resp = requests.get(url)
-                                if resp.status_code == requests.codes.ok:
-                                    resp = requests.get('https://api.cryptonator.com/api/ticker/' + coin1 + '-' + coin2)
-                                    if(resp.status_code == requests.codes.ok):
-                                        resp = resp.json()
-                                        #print '\033[91m ' + str(resp) + ' \033[0m'
-                                        response_text =  '\nThe current price of ' + coin1.upper() + ' is ' + resp['ticker']['price'] + ' ' + coin2.upper()
-                                        response_text += '\nCurrent Volume of the last 24 hours is ' + resp['ticker']['volume'] + ' ' + coin1.upper() + '(' + str(float(resp['ticker']['price']) * float(resp['ticker']['volume'])) + ' ' + coin2.upper() + ')'
+                                if not timeframe in ['24h', '7d', '30d', '1y']:
+                                    response_text = 'Invalid time frame sir. , the available options are : [24h, 7d, 30d, 1y]\n.'
+                                    response_text += 'Please ask me more by typing `@' + BOT_NAME + ' help`'
+                                else: #tries to get image
+                                  #try:
+                                    #building url
+                                    url = 'https://cryptohistory.org/charts/candlestick/'
+                                    url += coin1 + '-' + coin2 + '/' + timeframe + '/png'
+                                    resp = requests.get(url)
+                                    if resp.status_code == requests.codes.ok:
+                                        resp = requests.get('https://api.cryptonator.com/api/ticker/' + coin1 + '-' + coin2)
+                                        if(resp.status_code == requests.codes.ok):
+                                            resp = resp.json()
+                                            #print '\033[91m ' + str(resp) + ' \033[0m'
+                                            response_text =  '\nThe current price of ' + coin1.upper() + ' is ' + resp['ticker']['price'] + ' ' + coin2.upper()
+                                            response_text += '\nCurrent Volume of the last 24 hours is ' + resp['ticker']['volume'] + ' ' + coin1.upper() + '(' + str(float(resp['ticker']['price']) * float(resp['ticker']['volume'])) + ' ' + coin2.upper() + ')'
+                                        else:
+                                            response_text = 'Current Price and Volume are not available, but I have the graph, sir.'
+                                        title = coin1.upper() + ' - ' + coin2.upper() + ' '
+                                        if (timeframe == '24h'):
+                                            title += '24 Hours'
+                                        elif (timeframe == '7d'):
+                                            title += '7 Days'
+                                        elif (timeframe == '30d'):
+                                            title += '30 Days'
+                                        elif (timeframe == '7d'):
+                                            title += '1 Year'
+                                        else:
+                                            title += 'Invalid Timeframe [please contact my developer to fix this]'
+                                        title += ' graph'
+                                        params['attachments'] = json.dumps([
+                                            {
+                                                'pretext' : response_text,
+                                                'fallback': 'Crypto Graph',
+                                                'color': '#36a64f',
+                                                'title': title,
+                                                'image_url': url,
+                                                'thumb_url': url
+                                            }
+                                        ])
+                                        #print str(params)
                                     else:
-                                        response_text = 'Current Price and Volume are not available, but I have the graph, sir.'
-                                    title = coin1.upper() + ' - ' + coin2.upper() + ' '
-                                    if (timeframe == '24h'):
-                                        title += '24 Hours'
-                                    elif (timeframe == '7d'):
-                                        title += '7 Days'
-                                    elif (timeframe == '30d'):
-                                        title += '30 Days'
-                                    elif (timeframe == '7d'):
-                                        title += '1 Year'
-                                    else:
-                                        title += 'Invalid Timeframe [please contact my developer to fix this]'
-                                    title += ' graph'
-                                    params['attachments'] = json.dumps([
-                                        {
-                                            'pretext' : response_text,
-                                            'fallback': 'Crypto Graph',
-                                            'color': '#36a64f',
-                                            'title': title,
-                                            'image_url': url,
-                                            'thumb_url': url
-                                        }
-                                    ])
-                                    #print str(params)
-                                else:
-                                    response_text = 'Excuse me sir, but I can\'t find the coin pair you are asking for.\n'
-                                    response_text += 'Please have in mind that I get data from Poloniex archives.'
-                                    params['text'] = response_text
-                                resp = requests.post('https://slack.com/api/chat.postMessage', params=params)
-                              #except Exception as ex:
-                              #  print ex
+                                        response_text = 'Excuse me sir, but I can\'t find the coin pair you are asking for.\n'
+                                        response_text += 'Please have in mind that I get data from Poloniex archives.'
+                                        params['text'] = response_text
+                                    resp = requests.post('https://slack.com/api/chat.postMessage', params=params)
+                                  #except Exception as ex:
+                                    #  print ex
+                            else:
+                              params['text'] = 'Sorry sir. it seems that you want a graph but you don\'t provide me enough info.\n Check `@' + BOT_NAME + ' help` for info'
+                              resp = requests.post('https://slack.com/api/chat.postMessage', params=params)
                         #except Exception as ex:
                             #print ex
                     elif 'thank you' in received_text or 'thanks' in received_text:
